783be7006940af9c2fc2769d1b8bcc247f47e04f
gui7x <guilherme.moura@nave.org.br>
2026-05-28 08:55:22 -0300

    ....

--- src/respostas.js
+++ src/respostas.js
@@ -3,26 +3,30 @@
   * para selecionar na tabela cliente todos os clientes 
   * que moram em Jaboatão e Olinda
   */
-export const r1 = ``
+export const r1 = export const r1 = SELECT from clientes; where cidade in
+('Jaboatão', 'Olinda')
 
 /* Exercício 2:
   * Escreva entre as áspas da variável "r2" o comando 
   * para ordenar a tabela de produtos a partir do preço 
   * de cada item, em ordem do mais caro par ao mais barato
   */
-export const r2 = ``
+export const r2 = SELECT from produtos 
+where preco <= 999999
+order by preco desc;
 
 /* Exercício 3:
   * Escreva entre as áspas da variável "r3" o comando 
   * para filtrar os pedidos entre os meses de Outubro (10)
   * e Novembro (11)
   */
-export const r3 = ``
+export const r3 = SELECT from pedidos
+where data_pedidos BETWEEN '2025/12/12' AND '2025/12/18'
 
 /* Exercício 1:
   * Escreva entre as áspas da variável "r4" o comando 
   * para juntar informações da tabelas de produtos com
   * a tabela de pedidos
   */
-export const r4 = ``
+export const r4 = SELECT FROM produtos INNER JOIN pedidos ON produtos.id_produto = pedidos.id_produto;
 
