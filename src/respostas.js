4124555227dc9cc23106df7a34b3791b21db6ba2
Yggdrasil-Lrss <lucas.renato@nave.org.br>
2026-05-29 09:05:08 -0300

    Quase completo

--- src/respostas.js
+++ src/respostas.js
@@ -1,28 +1,48 @@
 /* Exercício 1:
-  * Escreva entre as áspas da variável "r1" o comando 
-  * para selecionar na tabela cliente todos os clientes 
+  * Escreva entre as áspas da variável "r1" o comando
+  * para selecionar na tabela cliente todos os clientes
   * que moram em Jaboatão e Olinda
   */
-export const r1 = ``
+export const r1 =
+`
+SELECT nome, telefone, cidade
+from clientes
+where cidade in
+('Jaboatão', ' Olinda');
+`
 
 /* Exercício 2:
-  * Escreva entre as áspas da variável "r2" o comando 
-  * para ordenar a tabela de produtos a partir do preço 
+  * Escreva entre as áspas da variável "r2" o comando
+  * para ordenar a tabela de produtos a partir do preço
   * de cada item, em ordem do mais caro par ao mais barato
   */
-export const r2 = ``
+export const r2 =
+`
+SELECT nome, preco, descricao
+from produtos prod
+where preco > 895
+and preco < 3800
+order by preco dec;
+`
 
 /* Exercício 3:
   * Escreva entre as áspas da variável "r3" o comando 
-  * para filtrar os pedidos entre os meses de Outubro (10)
-  * e Novembro (11)
+  * para filtrar os pedidos entre os meses de Outubro (12)
+  * e Novembro (18)
   */
-export const r3 = ``
+export const r3 = 
+`
+SELECT data_pedido from pedidos
+WHERE data_pedido betwen
+'2025-12-12' and '2025-12-18'
+`
 
-/* Exercício 1:
+/* Exercício 4:
   * Escreva entre as áspas da variável "r4" o comando 
   * para juntar informações da tabelas de produtos com
-  * a tabela de pedidos
+  * a tabela de itens_pedidos
   */
-export const r4 = ``
+export const r4 = 
+`
 
+`
