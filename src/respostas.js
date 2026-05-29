60419b2afcfaddc77f8b932a8b80f2ae82d335f0
Pablos <pablo.alves@nave.org.br>
2026-05-29 09:07:33 -0300

    atualizações feitas, mas não completo.

--- src/respostas.js
+++ src/respostas.js
@@ -3,9 +3,10 @@
   * para selecionar na tabela cliente todos os clientes
   * que moram em Jaboatão e Olinda
   */
-export const r1 = `select nome, cidade
-from clientes 
-where cidade in ('Jaboatão dos Gurarrapes', ‘Olinda’);
+export const r1 = `select nome,
+ cidade from clientes 
+where cidade in 
+('Jaboatão dos Gurarrapes', ‘Olinda’);
 `
 
 
@@ -30,9 +31,26 @@ between '2025-12-12' and '2025-12-18';`
 
 /* Exercício 4:
   * Escreva entre as áspas da variável "r4" o comando
-  * para juntar informações da tabelas de produtos com
-  * a tabela de pedidos
+  * para juntar informações da tabela de produtos com
+  * a tabela itens_pedido
   */
-export const r4 = ``
+export const r4 = `select 
+pr.nome,
+pr.vendedor,
+pr.preco,
+pr.descricao,
+pr.categoria,
+pr.id as id_produto,
+ip.pedido_id, 
+ip.produto_id,
+ip.quantidade,
+ip.preco_unitario
+from produtos pr 
+left JOIN itens_pedido ip
+on ip.produto_id = pr.id;`
 
+//produtos = id,nome,vendedor,preco,
+// descricao, categoria       
+//  itens_pedidos= id, pedido_id,
+// produto_id, quantidade, preco_unitario
 
