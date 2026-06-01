96243cf73ea56a9b933a8d248b74f42c8604c5f5
marapessoa06 <samara.pessoa@nave.org.br>
2026-06-01 08:07:25 -0300

    .

--- src/respostas.js
+++ src/respostas.js
@@ -3,26 +3,25 @@
   * para selecionar na tabela cliente todos os clientes 
   * que moram em Jaboatão e Olinda
   */
-export const r1 = ``
+export const r1 = ` select nome from clientes where cidade in('Jaboatão dos Guararapes', 'Olinda');`
 
 /* Exercício 2:
   * Escreva entre as áspas da variável "r2" o comando 
   * para ordenar a tabela de produtos a partir do preço 
   * de cada item, em ordem do mais caro par ao mais barato
   */
-export const r2 = ``
+export const r2 = `select nome, preco, descricao from produtos order by preco desc; `
 
 /* Exercício 3:
   * Escreva entre as áspas da variável "r3" o comando 
   * para filtrar os pedidos entre os meses de Outubro (10)
   * e Novembro (11)
   */
-export const r3 = ``
+export const r3 = `select nome, data_pedido from pedidos where data_pedido between '2025-12-12' and '2025-12-18'; `
 
-/* Exercício 1:
+/* Exercício 4:
   * Escreva entre as áspas da variável "r4" o comando 
   * para juntar informações da tabelas de produtos com
   * a tabela de pedidos
   */
-export const r4 = ``
-
+export const r4 = ` `
