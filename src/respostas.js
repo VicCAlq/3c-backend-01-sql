dd4b3812806f368004802aff2649d3e01dd30e24
antoniodoib <antonio.fernando@nave.org.br>
2026-06-01 08:06:34 -0300

    att 1

--- src/respostas.js
+++ src/respostas.js
@@ -3,26 +3,47 @@
   * para selecionar na tabela cliente todos os clientes 
   * que moram em Jaboatão e Olinda
   */
-export const r1 = ``
+export const r1 = `select nome, cidade from clientes 
+WHERE cidade in('Jaboatão dos Guararapes', 'olinda');`
 
 /* Exercício 2:
   * Escreva entre as áspas da variável "r2" o comando 
   * para ordenar a tabela de produtos a partir do preço 
   * de cada item, em ordem do mais caro par ao mais barato
   */
-export const r2 = ``
+export const r2 = `select preco  from produtos 
+-- where preco <= 2000
+order by preco desc;`
 
 /* Exercício 3:
   * Escreva entre as áspas da variável "r3" o comando 
-  * para filtrar os pedidos entre os meses de Outubro (10)
-  * e Novembro (11)
+  * para filtrar os pedidos entre os dias 12 e 18 de 
+  * Dezembro
   */
-export const r3 = ``
+export const r3 = `select data_pedido from pedidos 
+WHERE data_pedido between '2025-12-12' and '2025-12-18'`
 
-/* Exercício 1:
+/* Exercício 4:
   * Escreva entre as áspas da variável "r4" o comando 
   * para juntar informações da tabelas de produtos com
-  * a tabela de pedidos
+  * a tabela itens_pedido
   */
-export const r4 = ``
+export const r4 = `select 
+p.nome,
+p.vendedor,
+p.preco,
+p.descricao,
+p.categoria
+from produtos p 
+inner join itens_pedido ip
+on p.pedido_id = p.id;
 
+
+
+select
+    p.nome,
+    count(p.id) as total_de_pedidos
+    from produtos p 
+    inner join itens_pedido ip 
+     on p.id = p.pedido_id
+     group by p.id;
